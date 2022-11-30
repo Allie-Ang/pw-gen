33224352484f8f82ab01f0712196879033a5652a
Allison Ang <allisonang21@gmail.com>
2022-11-30 15:25:18 -0800

    adding lenth function

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var userPassword = "";
 // Replaced front end values from user input
 var useLowerCase;
 var useUpperCase;
-var useNUmbers;
+var useNumbers;
 var useSpecChar;
 var passwordLength;
 
@@ -86,7 +86,7 @@ function userGeneratedchoices() {
     "Would you like your randomly generated password to include lower case letters? Click OK to include or CANCEL to exclude."
   );
   useUpperCase = confirm(
-    "Would you like your randomly generated password to include upper case letters? Click OK to include  or CANCEL to exclude."
+    "Would you like your randomly generated password to include upper case letters? Click OK to include or CANCEL to exclude."
   );
   useNumbers = confirm(
     "Would you like your randomly generated password to include numbers? Click OK to include or CANCEL to exclude."
@@ -94,10 +94,10 @@ function userGeneratedchoices() {
   useSpecChar = confirm(
     "Would you like your randomly generated password to include special chatacter? Click OK to include or CANCEL to exclude."
   );
-  ß;
+
   useLowerCase ? addUserGenChoices(lowerCase) : false;
   useUpperCase ? addUserGenChoices(upperCase) : false;
-  useNUmbers ? addUserGenChoices(num) : false;
+  useNumbers ? addUserGenChoices(num) : false;
   useSpecChar ? addUserGenChoices(specChar) : false;
 }
 
@@ -105,19 +105,26 @@ function addUserGenChoices(stringofChoices) {
   userSelectedChar = userSelectedChar.concat(stringofChoices);
 }
 
-function writePassword() {
-  userGeneratedchoices();
-  var userPasswordOutput = generatePassword(userSelectedChar);
-  console.log(userPasswordOutput);
-  var passwordTextBox = document.querySelector("#password");
-  userPasswordOutput.value = password;
+function getUserPasswordLength() {
+  lengthInput = prompt(
+    "How many characters would you like in your random password?",
+    "Enter an integer between 8 and 128"
+  );
+  passwordLength = Math.floor(Number(lengthInput));
+  if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
+    alert("You must enter an integer between 8 and 128!");
+    lengthPrompt();
+  } else {
+    confirm(
+      "Your randomly generated password will be " +
+        passwordLength +
+        " characters long."
+    );
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Write password to the #password input
-function writePassword() {
+function writePassword(...args: []) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;

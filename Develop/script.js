a9ca28f1ce49731f12bc10aa3d620ea5127d5404
Allison Ang <allisonang21@gmail.com>
2022-11-29 18:07:33 -0800

    base code

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,102 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var userSelectedChar = [];
+var userPassword = "";
 
-// Variables to hold user input
-var chosenLength;
-var pwLength;
-
-function promptLength() {
-  chosenLength = prompt("Enter a # between 8 and 128");
-  pwLength = Math.floor(Number(chosenLength));
-  if (pwLength < 8 || pwLength > 128 || !pwLength) {
-    alert("Must be a # between 8-128");
-    promptLength();
-  } else {
-    confirm("Your PW will be " + pwLength + " characters long");
-  }
-}
+///user input mock data
+var useLowerCase = false;
+var useUpperCase = true;
+var useNUmbers = true;
+var useSpecChar = false;
+var passwordLength = 55;
 
 // Variable Choices
-var upperCase = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
-var lowerCase = [abcdefghijklmnopqrstuvwxyz];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+"];
 
+function generatePassword(userSelectedChoices) {
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * userSelectedChoices.length);
+    userPassword += userSelectedChoices.substring(
+      randomNumber,
+      randomNumber + 1
+    );
+  }
 
-// Function for Uppercase
-function upperCaseInput(){
-   upperCaseInput = 
+  console.log(userPassword);
 }
 
+function userGeneratedchoices() {
+  useLowerCase ? addUserGenChoices(lowerCase) : false;
+  useUpperCase ? addUserGenChoices(upperCase) : false;
+  useNUmbers ? addUserGenChoices(num) : false;
+  useSpecChar ? addUserGenChoices(specChar) : false;
+
+  console.log(userSelectedChar);
+}
+
+function addUserGenChoices(stringofChoices) {
+  userSelectedChar = userSelectedChar.concat(stringofChoices);
+}
+
+function writePassword() {
+  userGeneratedchoices();
+  generatePassword(userSelectedChar);
+}
 
 // Write password to the #password input
 function writePassword() {
